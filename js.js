3860e747cf5404a04aa201efe2ad85fa6c071a85
Serena Stephens <stephensserena@gmail.com>
2019-05-14 20:20:56 -0700

    created user page

---
+++ js.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    $('.modal').modal();
+    $('.carousel').carousel();
+  });
+
+
+
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  });
+
+  var slider = document.getElementById('test-slider');
+  noUiSlider.create(slider, {
+   start: [20, 80],
+   connect: true,
+   step: 1,
+   orientation: 'horizontal', // 'horizontal' or 'vertical'
+   range: {
+     'min': 0,
+     'max': 100
+   },
+   format: wNumb({
+     decimals: 0
+   })
+  });
+        
+          
+        
